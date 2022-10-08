b9011ad0747b8fd4c35065d403a756149561e525
mario <mario>
2022-10-07 23:11:26 -0500

    m

--- next.config.js
+++ next.config.js
@@ -10,9 +10,9 @@ module.exports = {
                 {key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT'},
                 {key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token,X-Requested-With'},
 
-            ]
-         }
-        ]
+            ],
+         },
+        ];
         
-    }
-}
+    },
+};
